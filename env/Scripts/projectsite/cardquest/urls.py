9f1c7c2f16350db611cf646f30f38b9bc0168002
JovanTim <202280218@psu.palawan.edu.ph>
2023-12-15 15:14:10 +0800

    ccccc

--- env/Scripts/projectsite/cardquest/urls.py
+++ env/Scripts/projectsite/cardquest/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('trainer_list', TrainerList.as_view(), name="trainer-list"),
-    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add')
+    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add'),
+    path('trainer_list/<pk>', TrainerUpdateView.as_view(), name='trainer-update'),
+    path('trainer_list/<pk>/delete', TrainerDeleteView.as_view(), name='trainer-delete')
 ]
--- env/Scripts/projectsite/cardquest/views.py
+++ env/Scripts/projectsite/cardquest/views.py
@@ -30,3 +30,16 @@ class TrainerCreateView(CreateView):
     template_name = 'trainer_add.html'
     success_url = reverse_lazy('trainer-list')
 
+class TrainerUpdateView(UpdateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_edit.html'
+    success_url = reverse_lazy('trainer-list')
+    form_class = TrainerForm
+    template_name = 'trainer_edit.html'
+    success_url = reverse_lazy('trainer-list')
+
+class TrainerDeleteView(DeleteView):
+    model = Trainer
+    template_name = 'trainer_del.html'
+    success_url = reverse_lazy('trainer-list')
